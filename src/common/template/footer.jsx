21b8b10c92585a45be339f4fe3e25347d4bd8737
Nilsiitcho <nyson96@hotmail.com>
2019-12-20 00:15:00 -0300

    Criando esqueleto da aplicação

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2019
+            <a href='https://google.com' target='_blank'> BMS Global</a>
+        </strong>
+    </footer>
+)
