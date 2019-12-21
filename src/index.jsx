26164087fcbb08386209f25a7b35d7db93c17b50
Nilsiitcho <nyson96@hotmail.com>
2019-12-21 20:34:51 -0300

    Configurando Dashboard com Redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import promise from 'redux-promise'
+import reducers from './main/reducers'
 import App from './main/app'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const devTootls = window.__REDUX_DEVTOOLS_EXTENSION__
+    && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = applyMiddleware(promise)(createStore)(reducers, devTootls)
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('app'))
